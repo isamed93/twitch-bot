c6de4177918a092f37a22d1b93884930ab17b20d
Ire <isamed1993@gmail.com>
2020-10-03 18:23:06 +0100

    added simple commands plus a multitwitch set up

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Summary
-Basic twitch bot that will announce its presence in chat
+Basic twitch bot that will announce its presence in chat and performs a few simple commands
 
 # Set up
 Set up a virtual environment with the following:
@@ -14,5 +14,26 @@ This virtual environment needs the following environment variables:
  - BOT_PREFIX=! (or whatever other prefix you want)
  - CHANNEL=#<channel_name>
 
+# Commands
+## General
+ - discord
+ - engineer
+ - discordmute
+ - sens
+## Mod
+ - multitwitch
+ - multitwitch_off
+## User-related
+ - lurk
+ - bonk
+## Guild Wars 2
+ - info
+ - pve
+ - pvp
+ - wvw
+ - training
+
 # Notes
-This bot was developed by following the following [tutorial](https://dev.to/ninjabunny9000/let-s-make-a-twitch-bot-with-python-2nd8)
+This bot was developed by following this [tutorial](https://dev.to/ninjabunny9000/let-s-make-a-twitch-bot-with-python-2nd8)
+
+TwitchIO library can be found [here](https://twitchio.readthedocs.io/en/rewrite/twitchio.html#)
--- spire_bot.py
+++ spire_bot.py
@@ -10,13 +10,18 @@ bot = commands.Bot(
     initial_channels=[os.environ['CHANNEL']]
 )
 
+multitwitch_link = 'www.multitwitch.tv'
+multitwitch_on = False
+
 
 @bot.event
 async def event_ready():
     '''Called once when the bot goes online'''
+    # Print log to console
     print(f"{os.environ['BOT_NICK']} is online")
     ws = bot._ws  # this is only needed to send messages within event_ready
     await ws.send_privmsg(os.environ['CHANNEL'], f"/me has landed!")
+
 
 @bot.event
 async def event_message(ctx):
@@ -27,10 +32,99 @@ async def event_message(ctx):
     # Handle the commands of the message
     await bot.handle_commands(ctx)
 
-@bot.command(name='test')
-async def test(ctx):
-    await ctx.send('/me test passed!')
+#----------------------------------------
+#   Commands
+#----------------------------------------
+## General
+@bot.command(name='discord')
+async def comm_discord(ctx):
+    await ctx.send_me('Discord link: https://discord.gg/EksFrcd')
+
+
+@bot.command(name='engineer')
+async def comm_engineer(ctx):
+    await ctx.send_me('Spire graduated from Industrial Electronics and Automation Engineering (think robots) and she works as a Digital Signal Processing Engineer. Feel free to ask her about it!')
+
+
+@bot.command(name='discordmute')
+async def comm_discordmute(ctx):
+    await ctx.send_me('Discord is muted sometimes because not everyone is comfortable with having their voice on stream.')
+
+
+@bot.command(name='sens')
+async def comm_sens(ctx):
+    await ctx.send_me('DPI: 1500 | Ingame: 5 | Scoped: 40')
+
+
+## Mod
+@bot.command(name='multitwitch')
+async def comm_multitwitch(ctx):
+    global multitwitch_link
+    global multitwitch_on
+    # If the command is run by a mod
+    if (ctx.author.is_mod):
+        # If command is run with arguments, set up multitwitch
+        if(bool(ctx.content.replace("!multitwitch","").strip())):
+            multitwitch_link = multitwitch_link + ctx.content
+            multitwitch_link = multitwitch_link.replace('!multitwitch', '')
+            multitwitch_link = multitwitch_link.replace(' ', '/')
+            multitwitch_on = True
+            print(f"{ctx.author.name} has set the multitwitch to {multitwitch_link}")
+        if(multitwitch_on):
+            await ctx.send_me("Watch multiple PoVs at {}".format(multitwitch_link))
+    # If the command is run by anyone else
+    else:
+        if(multitwitch_on):
+            print(f"{ctx.author.name} has disabled multitwitch")
+            await ctx.send_me("Watch multiple PoVs at {}".format(multitwitch_link))
+
+
+@bot.command(name='multitwitchoff')
+async def comm_multitwitchoff(ctx):
+    global multitwitch_on
+    # If the command is run by a mod
+    if (ctx.author.is_mod):
+        multitwitch_on = False
+
+
+## User-related
+@bot.command(name='lurk')
+async def comm_lurk(ctx):
+    await ctx.send_me('Thanks for leaving a lurk, {}! See you around :)'.format(ctx.author.name))
+
+
+@bot.command(name='bonk')
+async def comm_bonk(ctx):
+    print(f"{ctx.author.name} has timed themselves out")
+    await ctx.timeout(ctx.author.name, 10)
+
+
+## Guild Wars 2
+@bot.command(name='info')
+async def comm_info(ctx):
+    await ctx.send_me('Here\'s a few commands for info on the different gamemodes: !pvp !pve !wvw !training')
+
+
+@bot.command(name='pve')
+async def comm_pve(ctx):
+    await ctx.send_me('Raid builds + guides + training: snowcrows.com  --Fractals builds + guides: discretize.eu -- Gear guide: https://wiki.guildwars2.com/wiki/User:Tanetris/So_You_Want_To_Gear_a_Character')
+
+
+@bot.command(name='pvp')
+async def comm_pvp(ctx):
+    await ctx.send_me('PvP builds + guides: godsofpvp.net')
+
+
+@bot.command(name='wvw')
+async def comm_wvw(ctx):
+    await ctx.send_me('WvW builds: general builds metabattle.com')
+
+
+@bot.command(name='training')
+async def comm_training(ctx):
+    await ctx.send_me('Raid training: https://snowcrows.com/raids/training/ Fractals training: https://discord.gg/2ReUP7p')
 
 
 if __name__ == "__main__":
+    ## Global parameters
     bot.run()
